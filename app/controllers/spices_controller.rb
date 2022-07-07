26024661c41bfc117bf4ab717722feef1da4e159
William Hwang <willhwang90@gmail.com>
2022-07-07 10:28:21 -0400

    commit tree b890cdb7e77d8af90380da624e9de402064933bc

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice, status: :ok
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+end
